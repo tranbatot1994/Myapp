482b94f14982b8c5251b4263e7e32efe19c15af3
tranbatot1994 <tranbatot1994@gmail.com>
2021-06-22 01:20:43 +0900

    new websit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Dfinity Việt Nam
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
