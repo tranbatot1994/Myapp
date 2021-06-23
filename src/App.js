3a4f693d4e989c4bf1f6141403bd5b9f1db1d4d0
tranbatot1994 <tranbatot1994@gmail.com>
2021-06-23 18:56:35 +0900

    edit ki tu

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Nhóm Facebok
+         Nhóm Facebook
         </a>
         <a
           className="App-link"
