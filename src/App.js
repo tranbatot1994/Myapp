8cca0ca0862742be418f07f3e3a66e7986d15335
tranbatot1994 <tranbatot1994@gmail.com>
2021-06-23 14:37:26 +0900

    sua loi thay doi giao dien

--- src/App.js
+++ src/App.js
@@ -5,27 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nhóm Facebook cộng đồng
-        </p> 
-=======
         <img src={logo} className="App-logo" alt="logo" />
-        <a href="https://www.facebook.com/groups/dfinity">Dfinity Việt Nam</a>
->>>>>>> 9a473ff77f18cc82892c8e4e5611be5f6cb599c8
-        
+        <p>
+          Tham gia nhóm cộng đồng Facebook
+        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/groups/dfinity"
           target="_blank"
           rel="noopener noreferrer"
         >
           Dfinity Việt Nam
-        </a> 
-        <a 
-        className="App-link"
-        href="https://t.me/joinchat/FgipHXLL0r0wMmZl">Nhóm telegram
+        </a>
+        <a
+          className="App-link"
+          href="https://t.me/joinchat/FgipHXLL0r0wMmZl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nhóm Telegram
         </a>
       </header>
     </div>
