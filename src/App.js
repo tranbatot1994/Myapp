81acd5d592757106f4baa6c941905938952ff947
tranbatot1994 <tranbatot1994@gmail.com>
2021-06-23 13:55:44 +0900

    thay doi giao dien va cung cap them link telegram

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dfinity Việt Nam
-        </p>
+          Nhóm Facebook cộng đồng
+        </p> 
         
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dfinity Việt Nam
+        </a> 
+        <a 
+        className="App-link"
+        href="https://t.me/joinchat/FgipHXLL0r0wMmZl">Nhóm telegram
+        </a>
       </header>
     </div>
   );
